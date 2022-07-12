b28778626a617494d681cf25a46c32dc65476a7c
hoangnguyen <nvanhoang188@gmail.com>
2022-07-12 13:07:08 +0700

    update

--- resources/views/components/common/post.blade.php
+++ resources/views/components/common/post.blade.php
@@ -108,60 +108,60 @@
             </div>
         </div>
     </div>
-</article>
-<div class="modal fade" id="post-modal-{{ $post->id }}" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-body" style="min-height: 80vh;"></div>
+    <div class="modal fade" id="post-modal-{{ $post->id }}" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-body" style="min-height: 80vh;"></div>
+            </div>
         </div>
     </div>
-</div>
 
-@php
-$images = $post->images
-    ->map(function ($image) {
-        $img = $image->filename;
-        return [
-            'src' => url('/uploads/' . $img),
-            'responsive' => url('/uploads/' . $img),
-            'thumb' => url('/uploads/' . $img),
-        ];
-    })
-    ->toArray();
-@endphp
+    @php
+        $images = $post->images
+            ->map(function ($image) {
+                $img = $image->filename;
+                return [
+                    'src' => url('/uploads/' . $img),
+                    'responsive' => url('/uploads/' . $img),
+                    'thumb' => url('/uploads/' . $img),
+                ];
+            })
+            ->toArray();
+    @endphp
 
-<script>
-    $(function() {
-        const container = document.querySelector("#post-modal-{{ $post->id }} .modal-body");
+    <script>
+        $(function() {
+            const container = document.querySelector("#post-modal-{{ $post->id }} .modal-body");
 
-        const inlineGallery = lightGallery(container, {
-            container: container,
-            dynamic: true,
-            // Turn off hash plugin in case if you are using it
-            // as we don't want to change the url on slide change
-            hash: false,
-            // Do not allow users to close the gallery
-            closable: false,
-            // Add maximize icon to enlarge the gallery
-            showMaximizeIcon: true,
-            // Append caption inside the slide item
-            // to apply some animation for the captions (Optional)
-            appendSubHtmlTo: ".lg-item",
-            // Delay slide transition to complete captions animations
-            // before navigating to different slides (Optional)
-            // You can find caption animation demo on the captions demo page
-            slideDelay: 400,
-            dynamicEl: JSON.parse('<?php echo json_encode($images); ?>'),
+            const inlineGallery = lightGallery(container, {
+                container: container,
+                dynamic: true,
+                // Turn off hash plugin in case if you are using it
+                // as we don't want to change the url on slide change
+                hash: false,
+                // Do not allow users to close the gallery
+                closable: false,
+                // Add maximize icon to enlarge the gallery
+                showMaximizeIcon: true,
+                // Append caption inside the slide item
+                // to apply some animation for the captions (Optional)
+                appendSubHtmlTo: ".lg-item",
+                // Delay slide transition to complete captions animations
+                // before navigating to different slides (Optional)
+                // You can find caption animation demo on the captions demo page
+                slideDelay: 400,
+                dynamicEl: JSON.parse('<?php echo json_encode($images); ?>'),
 
-            // Completely optional
-            // Adding as the codepen preview is usually smaller
-            // thumbWidth: 60,
-            // thumbHeight: "40px",
-            // thumbMargin: 4
+                // Completely optional
+                // Adding as the codepen preview is usually smaller
+                // thumbWidth: 60,
+                // thumbHeight: "40px",
+                // thumbMargin: 4
+            });
+
+            setTimeout(() => {
+                inlineGallery.openGallery();
+            }, 200);
         });
-
-        setTimeout(() => {
-            inlineGallery.openGallery();
-        }, 200);
-    });
-</script>
+    </script>
+</article>
